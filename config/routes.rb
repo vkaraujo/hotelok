267a3a5a81f5be12a00360139ad992a1ac2ac82f
Viktor Araujo <vkaraujo00@gmail.com>
2021-03-03 19:10:10 -0300

    MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/amenidades', to: 'pages#amenidades'
+  get '/contatos', to: 'pages#contatos'
+  get '/eventos', to: 'pages#eventos'
+  get '/quartos', to: 'pages#quartos'
+  get '/sobrenos', to: 'pages#sobrenos'
 end
